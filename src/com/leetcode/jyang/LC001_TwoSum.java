243d61f2e3a244d473c01f10270eab41cdb94e26
yaliu <yaliu@ebay.com>
2015-02-26 14:34:55 -0800

    adding solutions for problem 2 and 3

--- src/com/leetcode/jyang/LC001_TwoSum.java
+++ src/com/leetcode/jyang/LC001_TwoSum.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * LEVEL: MEDIUM
+ * 
  * Given an array of integers, find two numbers such that they add up to a specific target number.
  *
  * The function twoSum should return indices of the two numbers such that they add up to the target, 
