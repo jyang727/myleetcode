a5c4befb909b57e3659bca1c8c7f619cef8a8aa8
yaliu <yaliu@ebay.com>
2015-02-27 16:08:40 -0800

    adding solutions for problem 6

---
+++ src/com/leetcode/jyang/LC_TestBed.java
@@ -0,0 +1,28 @@
+package com.leetcode.jyang;
+
+public class LC_TestBed {
+
+	public static void main(String[] args) {
+		
+	   	char[][] matrix = new char[3][5];
+	   	
+//	   	for (int i=0; i<matrix.length; i++){
+//	   		
+//	   		for (int j=0; j<matrix[i].length; j++){
+//	   			matrix[i][j]= 'A';
+//	   		}
+//	   	}
+	   	
+	   	for (int i=0; i<matrix.length; i++){
+	   		
+	   		for (int j=0; j<matrix[i].length; j++){
+	   			System.out.print((int)(matrix[i][j]) + " ");
+	   		}
+	   		System.out.println();
+	   	}
+		
+	   	
+	   	
+	}
+	
+}
